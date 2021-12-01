c077f0db4033052b2f0c6acb935f2981a1735553
Sven W <svenwml@yahoo.de>
2021-12-01 22:44:32 +0100

    add day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,46 @@
 fun main() {
+
+    fun calculate_number_of_increases_in_list(input: List<String>): Int {
+        var previous_measurement = Int.MAX_VALUE
+        var measurement_increase_counter = 0
+
+        for(measurement in input)
+        {
+            if(measurement.toInt() > previous_measurement)
+            {
+                measurement_increase_counter++
+            }
+
+            previous_measurement = measurement.toInt()
+        }
+
+        return measurement_increase_counter
+    }
+
+    fun generate_measurement_window_sum_list(input: List<String>, window_size: Int): List<String> {
+        val windows = input.windowed(window_size,1)
+
+        val sums_list = arrayListOf<String>()
+        for (window in windows.toList())
+        {
+            var sum = 0
+            for(i in 0..window_size - 1) {
+                sum += window[i].toInt();
+            }
+
+            sums_list.add(sum.toString())
+        }
+
+        return sums_list
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return calculate_number_of_increases_in_list(input)
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return calculate_number_of_increases_in_list(generate_measurement_window_sum_list(input, 3))
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
